4efdbf8760ea6abade60ce4f2f2f430439f04618
Nguyen Duy Son <uchihason5@gmail.com>
2026-05-14 17:59:09 +0700

    done QueryProduct

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -5,7 +5,8 @@ import { type QueryProductType } from 'src/types/request';
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
-  @Get() // GET /products?keyword=iphone&minPrice=100
+  //GET products?keyword=iphone&categoryId=1&minPrice=100&maxPrice=1000&page=1&limit=10
+  @Get()
   findAll(@Query() query: QueryProductType) {
     return this.productsService.findAll(query);
   }
