1518f1f7d8b2dfd6baf264019a9f6e50ed21bed6
Nguyen Duy Son <uchihason5@gmail.com>
2026-05-15 16:29:41 +0700

    Block guard for Product

--- src/admin/products/products.controller.ts
+++ src/admin/products/products.controller.ts
@@ -20,35 +20,36 @@ import {
 } from './dto/product.dto';
 
 @Controller('admin/products')
-// @UseGuards(AuthGuard)
+@UseGuards(AuthGuard)
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
   //GET products?keyword=iphone&categoryId=1&minPrice=100&maxPrice=1000&page=1&limit=10
-  // @UseGuards(PermissionsGuardMixin('product:read'))
+  @UseGuards(PermissionsGuardMixin('product:read'))
   @Get()
   findAll(@Query() query: QueryProductType) {
     return this.productsService.findAll(query);
   }
+  @UseGuards(PermissionsGuardMixin('product:read'))
   @Get(':id')
   findOne(@Param('id') id: number) {
     return this.productsService.findOne(+id);
   }
-
+  @UseGuards(PermissionsGuardMixin('product:create'))
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productsService.createProduct(createProductDto);
   }
-
+  @UseGuards(PermissionsGuardMixin('product:update'))
   @Put('/:id')
   update(@Param('id') id: number, @Body() updateProductDto: UpdateProductDto) {
     return this.productsService.updateProduct(+id, updateProductDto);
   }
-
+  @UseGuards(PermissionsGuardMixin('product:delete'))
   @Delete('/:id')
   delete(@Param('id') id: number) {
     return this.productsService.deleteProduct(+id);
   }
-
+  @UseGuards(PermissionsGuardMixin('product:update'))
   @Put('/:id/status')
   updateStatus(@Param('id') id: number, @Body() updateStatus: UpdateStatusDto) {
     return this.productsService.updateStatus(+id, updateStatus);
